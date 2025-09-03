f1c093d8561d64d10bd59474993f18a6945a8106
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-03 16:41:03 +0530

    Basic Programs

--- Basic/Pointers.c
+++ Basic/Pointers.c
@@ -9,9 +9,9 @@ int main()
     q = &b;
     printf("First Integer = %d \n", a);
     printf("Value of First Integer at it's address = %d \n", *p);
-    printf("Adress Of First Integer = %d \n", p);
+    printf("Address Of First Integer = %d \n", p);
     printf("Second Integer  =  %d \n", b);
     printf("Value of Second Integer at it's address = %d \n");
-    printf("Adress Of Second Integer = %d \n", q);
+    printf("Address Of Second Integer = %d \n", q);
     return 0;
 }
