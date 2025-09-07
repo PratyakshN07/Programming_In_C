3296d26d534396a75189b25f573e1d6f82eb976e
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-07 14:04:09 +0530

    If_Else Programs

---
+++ If_Else/Divisibleby3or5butnot15.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter Number = ");
+    scanf("%d", &n);
+    if (n % 3 == 0 || n % 5 == 0)
+    {
+        if (n % 15 != 0)
+        {
+            printf("Number is divisivle by 3 or 5 but not 15");
+        }
+        else
+        {
+            printf("The number is divisible by 15");
+        }
+    }
+    else
+    {
+        printf("Number is not divisible by 3 or 5");
+    }
+    return 0;
+}
