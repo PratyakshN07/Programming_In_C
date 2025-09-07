9a481460c5d723ba511a701d4c3437665dac19e5
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-09-07 18:09:07 +0530

    Ternary_Operator

---
+++ If_Else/Ternary_Odd_Even.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number = ");
+    scanf("%d", &n);
+    // ternary operator
+    // expression 1 ? expression 2 : expression 3 ;
+    n % 2 == 0 ? printf("It's a even number") : printf("It's a odd number");
+    return 0;
+}
