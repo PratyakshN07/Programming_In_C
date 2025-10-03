7c9675ef69424b9f45e85f6f8f40c571d486378a
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-10-03 20:35:09 +0530

    Loops

--- Loops/Defined_table_of_any_number.c
+++ Loops/Defined_table_of_any_number.c
@@ -2,9 +2,10 @@
 int main()
 {
     int n;
-    printf("Enter a number = ");
+    printf("Enter a number =");
     scanf("%d", &n);
     for (int i = 1; i <= 10; i++)
         printf("%d X %d = %d \n", n, i, n * i);
     return 0;
+
 }
