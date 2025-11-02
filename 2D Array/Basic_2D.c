f6985e5291260e1ccb469daeb46bfc9392ce607d
Pratyaksh Negi <Pratyakshn1@gmail.com>
2025-11-02 14:26:10 +0530

    2D Array

---
+++ 2D Array/Basic_2D.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[2][2] = {
+        {1, 2},
+        {3, 4},
+    };
+    for (int i = 0; i <= 1; i++)
+    {
+        for (int j = 0; j <= 1; j++)
+        {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
